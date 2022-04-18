46712a034255f9187b86cec41131b598702e6cd1
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2022-04-18 15:52:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,66 @@ function startClassification (){
 function modelready(){
     classifier.classify(gotResults);
 }
+function gotResults(error, results) {
+
+    if (error) {
+
+        console.error(error);
+
+        
+    } else {
+     console.log(results);
+     r=Math.floor(Math.random()* 255)+ 1;  
+     g=Math.floor(Math.random()* 255)+ 1;  
+     b=Math.floor(Math.random()* 255)+ 1;
+     document.getElementById("result_label").innerHTML="I can hear - "+results[0].label;  
+     document.getElementById("result_confidence").innerHTML="Accuracy - "+results[0].confidence.toFixed(1)+"%";
+     document.getElementById("result_label").style.color="rgb("+r+","+g+","+b+")";
+     document.getElementById("result_confidence").style.color="rgb("+r+","+g+","+b+")";
+     img1=document.getElementById("lion.webp");
+     img2=document.getElementById("sheep.jpg");
+     img3=document.getElementById("goat.jpg");
+     img4=document.getElementById("dog.jpg");
+     img4=document.getElementById("wolf.jpg");
+    if (results[0].label=="Roar") {
+        img1.src="simba roar.gif";  
+        img2.src="goat.jpg";  
+        img3.src="wolf.jpg";  
+        img4.src="dog.jpg";
+        img5.src="sheep.jpg";  
+          
+        }
+        if (results[0].label=="goat") {
+            img1.src="lion.webp";  
+            img2.src="baa goat.gif";  
+            img3.src="wolf.jpg";  
+            img4.src="dog.jpg";
+            img5.src="sheep.jpg";  
+              
+            }
+            if (results[0].label=="wolf") {
+                img1.src="lion.webp";  
+                img2.src="goat.jpg";  
+                img3.src="howl-wolf.gif";  
+                img4.src="dog.jpg";
+                img5.src="sheep.jpg";  
+                  
+                }
+                if (results[0].label=="dog") {
+                    img1.src="lion.webp";  
+                    img2.src="goat.jpg";  
+                    img3.src="wolf.jpg";  
+                    img4.src="dog bark.gif";
+                    img5.src="sheep.jpg";  
+                      
+                    }
+                    if (results[0].label=="sheep") {
+                        img1.src="lion.webp";  
+                        img2.src="goat.jpg";  
+                        img3.src="wolf.jpg";  
+                        img4.src="dog.jpg";
+                        img5.src="sheep baa.gif";  
+                          
+                        }
+          }
+}
